4896cf2193d4c9a141f2e7a079cb888aa60b994a
TJ Phippen <tj@moorhousecoating.com>
2019-08-31 03:46:19 -0600

    update Questionnaire.php

--- src/Models/Questionnaire.php
+++ src/Models/Questionnaire.php
@@ -20,9 +20,13 @@ class Questionnaire extends Model implements QuestionnaireContract
         'created_at', 'updated_at', 'deleted_at',
     ];
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-      'name',
-      'options'
+        'name', 'options'
     ];
 
     public function questions(): HasMany
